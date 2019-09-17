15a2b9dc2c5b5a932ed9f7ebd5b1546a8c8231cf
Brian Prescott <bp@wave39.com>
2019-09-17 10:30:24 -0400

    334

--- 334. Increasing Triplet Subsequence.playground/Contents.swift
+++ 334. Increasing Triplet Subsequence.playground/Contents.swift
@@ -3,4 +3,26 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+func hIndex(_ citations: [Int]) -> Int {
+    if citations.count == 0 {
+        return 0
+    }
+    
+    var hIndex = 0
+    let citationMax = citations.max()!
+    if citationMax == 0 {
+        return 0
+    }
+    
+    for n in 1...citationMax {
+        let aboveCount = citations.filter{$0 >= n}.count
+        if aboveCount >= n {
+            hIndex = n
+        }
+    }
+    
+    return hIndex
+}
+
+hIndex([0, 1, 0])
+hIndex([3, 0, 1, 2, 5, 6])
