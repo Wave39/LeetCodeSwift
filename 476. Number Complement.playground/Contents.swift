771c2fb22e3c25390c75d3efb954c3c346164b16
Brian Prescott <bp@wave39.com>
2019-09-17 17:36:04 -0400

    476

--- 476. Number Complement.playground/Contents.swift
+++ 476. Number Complement.playground/Contents.swift
@@ -3,4 +3,19 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+func findComplement(_ num: Int) -> Int {
+    let binaryString = String(num, radix: 2)
+    var complementString = ""
+    for c in binaryString {
+        if c == "1" {
+            complementString += "0"
+        } else {
+            complementString += "1"
+        }
+    }
+    
+    return Int(complementString, radix: 2)!
+}
+
+findComplement(5)       // 2
+findComplement(1)       // 0
