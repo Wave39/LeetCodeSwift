58328b1cca3b79b9ec0691e67b2ef16518e8047f
Brian Prescott <bp@wave39.com>
2019-09-17 17:23:19 -0400

    304

--- 304. Range Sum Query 2D - Immutable.playground/Contents.swift
+++ 304. Range Sum Query 2D - Immutable.playground/Contents.swift
@@ -3,4 +3,32 @@
 
 import Cocoa
 
-var str = "Hello, playground"
+class NumMatrix {
+    
+    var m: [[Int]]
+    init(_ matrix: [[Int]]) {
+        m = matrix
+    }
+    
+    func sumRegion(_ row1: Int, _ col1: Int, _ row2: Int, _ col2: Int) -> Int {
+        var t = 0
+        for r in row1...row2 {
+            for c in col1...col2 {
+                t = t + m[r][c]
+            }
+        }
+        
+        return t
+    }
+}
+
+let matrix = NumMatrix([
+    [3, 0, 1, 4, 2],
+    [5, 6, 3, 2, 1],
+    [1, 2, 0, 1, 5],
+    [4, 1, 0, 1, 7],
+    [1, 0, 3, 0, 5]
+])
+matrix.sumRegion(2, 1, 4, 3) // 8
+matrix.sumRegion(1, 1, 2, 2) // 11
+matrix.sumRegion(1, 2, 2, 4) // 12
