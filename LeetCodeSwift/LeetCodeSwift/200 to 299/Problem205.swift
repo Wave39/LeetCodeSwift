05085fe46a1c890122a2169eeebf94f2f7a74556
Brian Prescott <bp@wave39.com>
2022-09-02 23:09:53 -0400

    205

---
+++ LeetCodeSwift/LeetCodeSwift/200 to 299/Problem205.swift
@@ -0,0 +1,48 @@
+//
+//  Problem205.swift
+//  LeetCodeSwift
+//
+//  Created by Brian Prescott on 9/2/22.
+//
+
+// 205. Isomorphic Strings
+// https://leetcode.com/problems/isomorphic-strings/
+
+public class Problem205 {
+    public func run() {
+        let solve1 = isIsomorphic("egg", "add")
+        print("\(solve1) -- should be true")
+        let solve2 = isIsomorphic("foo", "bar")
+        print("\(solve2) -- should be false")
+        let solve3 = isIsomorphic("paper", "title")
+        print("\(solve3) -- should be true")
+    }
+
+    public func test(_ s: String, _ t: String) -> Bool {
+        return isIsomorphic(s, t)
+    }
+
+    func isIsomorphic(_ s: String, _ t: String) -> Bool {
+        let sArray = Array(s) as! [Character]
+        let tArray = Array(t) as! [Character]
+        var indexSet = Set<Int>()
+        for idx in 0..<s.count {
+            indexSet.insert(idx)
+        }
+
+        while !indexSet.isEmpty {
+            let minEntry = indexSet.min()!
+            let sChar = sArray[minEntry]
+            let tChar = tArray[minEntry]
+            let sIdx = sArray.enumerated().compactMap { $1 == sChar ? $0 : nil }
+            let tIdx = tArray.enumerated().compactMap { $1 == tChar ? $0 : nil }
+            if !sIdx.elementsEqual(tIdx) {
+                return false
+            }
+
+            sIdx.forEach { indexSet.remove($0) }
+        }
+
+        return true
+    }
+}
--- LeetCodeSwift/LeetCodeSwiftTests/Problems200to299.swift
+++ LeetCodeSwift/LeetCodeSwiftTests/Problems200to299.swift
@@ -37,6 +37,13 @@ class Problems200to299: XCTestCase {
         XCTAssertEqual(Problem200().test(test2c), 3)
     }
 
+    func test_Problem205() throws {
+        XCTAssertTrue(Problem205().test("egg", "add"))
+        XCTAssertTrue(Problem205().test("paper", "title"))
+
+        XCTAssertFalse(Problem205().test("foo", "bar"))
+    }
+    
     func test_Problem234() throws {
         let solve1 = Problem234().test(ListNode.arrayToList([1, 2, 2, 1]))
         XCTAssertTrue(solve1)
